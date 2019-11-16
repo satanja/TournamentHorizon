3ecff5cdb0df6110799b751031165c088930c869
satanja <s.a.tanja@student.tue.nl>
2019-11-16 23:55:06 +0100

    Remove redundant code

--- Player.py
+++ Player.py
@@ -2,13 +2,4 @@ class Player:
     def __init__(self, name, id):
         self.name = name
         self.id = id
-        self.wins = 0
-        self.delta = 0
-    
-    def update(self, isWinner, score1, score2):
-        if isWinner:
-            self.wins += 1
-            self.delta += score1 - score2
-        else: 
-            self.delta += score2 - score1
     
